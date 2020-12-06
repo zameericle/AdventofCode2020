77b7589ea25ffb98c177430e9a046e275fd7dc23
zameericle <zameericle@gmail.com>
2020-12-05 17:20:23 -0800

    Completed Part2

---
+++ Day5/Part2.js
@@ -0,0 +1,70 @@
+fs = require('fs')
+
+var samples = []
+
+data = fs.readFileSync(process.env.INPUT_PATH, "utf8")
+data.split(/\n/).forEach((data) => {
+   samples.push(data)
+})
+
+//I saw later that one could convert FB and LR to binary using the following mappings:
+// F = 0
+// B = 1
+// L = 0
+// R = 1
+
+function calculateRow(rowSpecifier) {
+   let row = rowSpecifier.split("").reduce((acc, next) => {
+      switch (next) {
+         case "F":
+            return [acc[0], acc[0] + acc[1] >> 1] 
+            break
+         case "B":            
+            return [(acc[0] + acc[1] >> 1) + 1, acc[1]]
+            break
+      }
+
+   }, [0,127])
+
+   return row[0]
+}
+
+function calculateCol(colSpecifier) {
+   let col = colSpecifier.split("").reduce((acc, next) => {
+      switch (next) {
+         case "L":
+            return [acc[0], acc[0] + acc[1] >> 1] 
+            break
+         case "R":            
+            return [(acc[0] + acc[1] >> 1) + 1, acc[1]]
+            break
+      }
+
+   }, [0,7])
+
+   return col[0]  
+}
+
+let numPasses = samples.length
+
+let seatIds = samples.map((sample) => {
+   let re  = /^([F|B]{7})([L|R]{3})/
+   let match = sample.match(re)
+   return [calculateRow(match[1]), calculateCol(match[2])]
+})
+.map((seat) => {
+   return seat[0] * 8 + seat[1] 
+})
+.sort((a,b) => {
+   return a-b
+})
+.filter((val, idx, arr) => {
+   if ((idx+1) === numPasses) {
+      return false
+   }
+
+   return ((arr[idx+1] - arr[idx]) > 1) ? true : false
+})
+.reduce((acc, val) => {
+   return acc + val + 1
+}, 0)
