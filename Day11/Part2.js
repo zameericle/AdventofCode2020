76a6dac048b0f4731a36d15582520d1de1b2915e
zameericle <zameericle@gmail.com>
2020-12-13 10:43:31 -0800

    added Part2 - refactored Part1 a bit

---
+++ Day11/Part2.js
@@ -0,0 +1,175 @@
+fs = require("fs")
+
+function makeSeatIterator(seats, rw, adjStratFn) {
+   /* return the current seat + adjaceny matrix:
+      1 2 3
+      3 S 4
+      5 6 7 */
+   let rowWidth = rw
+   let data = [...seats]
+   let nextIdx = 0
+   let nextSeat = data[nextIdx]
+   let nextRow = parseInt(nextIdx / rowWidth)
+   let nextCol = nextIdx % rowWidth
+   let adjMatStrat = adjStratFn
+
+   const seatIterator = {
+      next: function() {
+         let result = {
+            done: (nextSeat === undefined) ? true : false,
+            seat: nextSeat,
+            row:  nextRow,
+            col:  nextCol,
+            idx: nextIdx,
+            adjMat: (nextSeat === undefined) ? [] : adjMatStrat(nextRow, nextCol)
+         }
+
+         nextIdx++
+         nextSeat = data[nextIdx]
+         nextRow = parseInt(nextIdx / rowWidth)
+         nextCol = nextIdx % rowWidth
+
+         return result
+      }
+   }
+
+   return seatIterator
+}
+
+function prettyPrint(seats, width) {
+   let str=""
+
+   seats.forEach((entry, idx) => {
+      str += entry
+      if ((idx % width) === (width - 1)) {
+         str += "\n"
+      }
+   })
+
+   console.log(str)
+}
+
+function performRound(seats, rowWidth) {
+   let numRows = parseInt(seats.length / rowWidth)
+
+   /* rowWidth-1, -rowWidth, -rowWidth+1, 
+                  -1, 0, 1, 
+      rowWidth-1, rowWidth, rowWidth+1 */ 
+   let adjacencyMatrixFn = function (row, col) {   
+      let currIdx = row * rowWidth + col
+      let offsets = [
+      -rowWidth-1, -rowWidth, -rowWidth+1, 
+      -1, 0, 1, 
+      rowWidth-1, rowWidth, rowWidth+1]
+
+      let adjMatrix = 
+      ['x','x','x',
+      'x','x','x',
+      'x','x','x']
+
+      offsets.forEach((offset, idx) => {
+         adjMatRowIdx = parseInt(idx / 3)
+         adjMatColIdx = idx % 3
+         let entry = seats[currIdx + offset] 
+
+         /* first row set the top row of the adjMatrix to 'x'*/
+         if (row === 0 && adjMatRowIdx === 0) {
+            entry = 'x'
+         }
+
+         /* bottom row set the bottom row of the adjMatrix to 'x'*/
+         if (row === (numRows-1) && adjMatRowIdx === 2) {
+            entry = 'x'
+         }
+
+         /* first col set the left column of the adjMatrix to 'x'*/
+         if (col === 0 && adjMatColIdx === 0) {
+            entry = 'x'
+         }
+
+         /* first col set the right column of the adjMatrix to -1*/
+         if (col === (rowWidth-1) && adjMatColIdx === 2) {
+            entry = 'x'
+         }
+
+         adjMatrix[idx] = entry  
+      })            
+
+      return adjMatrix
+   }
+
+   iter = makeSeatIterator(seats, rowWidth, adjacencyMatrixFn)
+   let seatInfo = iter.next()
+   let nextState = []
+   let updateSeat = false
+   let isUpdated = false
+
+   while (!seatInfo.done) {
+      updateSeat = false
+
+      switch(seatInfo.seat) {
+         case "L":
+            
+            let updateSeat = seatInfo.adjMat.filter((val) => {
+               return val === "#"
+            }).length === 0
+
+            nextState[seatInfo.idx] = updateSeat ? "#" : "L"
+            if (updateSeat) {
+               isUpdated = true
+            }
+            break
+
+         case "#":
+            let count = seatInfo.adjMat.reduce((cnt, val,idx) => {
+               if ((val === "#") && (idx != 4)) {
+                  cnt++
+               }
+
+               return cnt
+            },0)
+
+            nextState[seatInfo.idx] = (count >= 4) ? "L" : "#"
+            if (count >= 4) {
+               isUpdated = true   
+            }
+            break
+
+         case ".":
+            nextState[seatInfo.idx] = "."
+            break
+         case "x":
+            break
+      }
+      
+      seatInfo = iter.next()
+   }      
+
+   return [isUpdated, nextState]
+}
+
+
+let seats = "L.LL.LL.LLLLLLLLL.LLL.L.L..L..LLLL.LL.LLL.LL.LL.LLL.LLLLL.LL..L.L.....LLLLLLLLLLL.LLLLLL.LL.LLLLL.LL"
+   .split("")
+// let seats = fs.readFileSync("/Users/zameericle/Development/AdventofCode2020/Day11/input.txt", "utf8").split("")
+let rowWidth = 10
+
+let nextState = performRound(seats, rowWidth)
+
+let run = 0
+
+while(nextState[0]) {
+   console.log("Round " + ++run)
+   nextState = performRound(nextState[1], rowWidth) 
+}
+
+let occupiedSeats = nextState[1].reduce((acc, val) => {
+   if (val === "#") {
+      acc++
+   }
+   return acc
+
+}, 0)
+
+prettyPrint(nextState[1],rowWidth)
+console.log(occupiedSeats)
